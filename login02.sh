0f7b53993aba28be80777d8610cd0b630fb71add
dupires <31322063+dupires@users.noreply.github.com>
2017-09-14 16:27:38 -0300

    Create login02.sh

---
+++ login02.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+clear
+echo
+echo 'Debian GNU/Linux Jessie 8 ttyl'
+echo
+echo -n 'Jessie Login:'
+read LOGIN
+echo -s 'Password:'
+read PASSW
+echo
+if [ $LOGIN == 'vangrat' ] ; then
+if [ $PASSW == '123' ] ; then 
+ echo 
+
